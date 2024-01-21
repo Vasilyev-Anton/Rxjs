b949524add1ccc640f2a25171820b4b2ce38f48b
Vasilyev-Anton <vasilyev1812@gmail.com>
2024-01-21 14:12:56 +0300

    upd url

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@ import {
 } from 'rxjs/operators';
 
 const messagesContainer = document.querySelector('.content-messages');
-const apiUrl = 'https://rxjs-backend.vercel.app/';
+const apiUrl = 'https://rxjs-backend-mb9z64egg-ants-projects-edd85abf.vercel.app/';
 
 const messageWidget = interval(5000).pipe(
   switchMap(() => ajax.getJSON(apiUrl).pipe(
